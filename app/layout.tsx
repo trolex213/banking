f8be9bab7c091bfe71e2b683515d25c02c9cc4cb
Troy Zhang <troyz0213@gmail.com>
2024-11-28 15:59:16 -0800

    global wrapper updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={'${inter.variable} ${ibmPlexSerif.variable}'}>{children}</body>
     </html>
   );
 }
